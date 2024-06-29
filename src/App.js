98dfb870d36ceca5018a92708f413785199e12be
luiswebneo <luisfernandolopes0@gmail.com>
2024-06-28 22:22:14 -0300

    começando o hero

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import Hero from './components/hero/Hero'
 import Navbar from './components/navbar/Navbar'
+
 
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
+  
       
     </div>
   );
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,11 +6,25 @@ import { BsFillHouseFill } from "react-icons/bs";
 import './Navbar.css'
 
 const Navbar = () => {
+
+    const[click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+
     return(
         <div className="navbar">
             <div className="container">
                 <h1><span><BsFillHouseFill />Real</span>Estate</h1>
                 <button className="btn">Sing In</button>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Search</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <div className="hamburguer" onClick={handleClick}>
+                    { click ? (<FaRegTimesCircle className="icon" />) : (<HiOutlineMenuAlt4  className="icon"/>)}
+
+                </div>
             </div>
         </div>
     )
