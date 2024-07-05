bbdaa7ff3f1467f6e47bb0cf0a937ef29d4f034c
luiswebneo <luisfernandolopes0@gmail.com>
2024-07-05 20:27:59 -0300

    Finalizado a seção

--- src/components/best/Best.js
+++ src/components/best/Best.js
@@ -1,9 +1,28 @@
 import React from "react";
 
+import Apt1 from '../../assets/apt1.jpeg'
+import Apt2 from '../../assets/apt2.jpeg'
+import Apt3 from '../../assets/apt3.jpeg'
+
+
+import './Best.css';
+
 const Best = () => {
     return(
-        <div>
-            <h1>ola mundo</h1>
+        <div className="best">
+            <h1>Find Best Rated Properties</h1>
+            <div>
+                <p><span className="bold">All</span></p>
+                <p>Comercial</p>
+                <p>Residential</p>
+                <p>Agricultural</p>
+            </div>
+            <div className="container">
+                <img src={Apt1} alt=""/>
+                <img src={Apt2} alt=""/>
+                <img src={Apt3} alt=""/>
+            </div>
+            <button className="btn">View All</button>
         </div>
     )
 }
