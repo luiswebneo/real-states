ef22c91325fdecd0def08d325c0e0f64e1ed697d
luiswebneo <luisfernandolopes0@gmail.com>
2024-07-04 22:00:34 -0300

    Começando a parte a section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from './components/hero/Hero'
 import Navbar from './components/navbar/Navbar'
-
+import Best from './components/best/Best';
 
 
 function App() {
@@ -9,8 +9,7 @@ function App() {
     <div>
       <Navbar />
       <Hero />
-  
-      
+      <Best />
     </div>
   );
 }
---
+++ src/components/best/Best.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Best = () => {
+    return(
+        <div>
+            <h1>ola mundo</h1>
+        </div>
+    )
+}
+
+export default Best;
