81de73c2ab80ea836a1e42e3bdeedf303fcafd61
luiswebneo <luisfernandolopes0@gmail.com>
2024-07-09 20:42:35 -0300

    finalizado

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Hero from './components/hero/Hero'
 import Navbar from './components/navbar/Navbar'
 import Best from './components/best/Best';
 import Featured from './components/featured/Featured';
+import Footer from './components/footer/Footer';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <Best />
       <Featured />
+      <Footer />
     </div>
   );
 }
